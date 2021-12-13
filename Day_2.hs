1aa0ea915cf1e1d7e6639a54f1ec8ebef332f65d
Ville Werner <ville.werner@gmail.com>
2021-12-14 01:27:39 +0200

    Small changes

--- Day_2.hs
+++ Day_2.hs
@@ -1,13 +1,11 @@
-import Control.Monad
-
 type Position = (Int,Int) -- (horizontal, vertical)
 type Position2 = (Int, Int, Int) -- (horizontal, vertical, aim)
 
 input :: IO [String]
-input = return . lines =<< readFile "day_2_input.txt"
+input = lines <$> readFile "day_2_input.txt"
 
 firstPart :: IO Int
-firstPart = liftM ((\ (a,b) -> a*b) . foldl move (0,0)) $ input
+firstPart = fmap ((uncurry (*)) . foldl move (0,0)) input
 
 move :: Position -> String -> Position
 move pos instr = moveCmd amount pos
@@ -17,6 +15,7 @@ move pos instr = moveCmd amount pos
       "forward" -> increaseHorizontal
       "down"    -> increaseVertical
       "up"      -> decreaseVertical
+      _         -> const id
 
 increaseHorizontal :: Int -> Position -> Position
 increaseHorizontal amount (h,v) = (h + amount, v)
@@ -28,19 +27,20 @@ decreaseVertical :: Int -> Position -> Position
 decreaseVertical amount (h,v) = (h, max (v - amount) 0)
 
 secondPart :: IO Int
-secondPart = liftM ((\ (a,b,c) -> a*b) . foldl move2 (0,0,0)) $ input
+secondPart = fmap ((\ (a,b,c) -> a*b) . foldl move2 (0,0,0)) input
 
 move2 :: Position2 -> String -> Position2
 move2 pos instr = moveCmd amount pos
   where
     (cmd, amount) = (\ [x,y] -> (x, read y :: Int)) $ words instr
     moveCmd = case cmd of
-      "forward" -> \ amount -> \ orig@(h,v,a) -> (adapt $ increaseVertical (a * amount)) . (adapt $ increaseHorizontal amount) $ orig
+      "forward" -> \ amount orig@(h,v,a) -> (adapt $ increaseVertical (a * amount)) . (adapt $ increaseHorizontal amount) $ orig
       "down"    -> increaseAim
       "up"      -> decreaseAim
+      _         -> const id
 
 adapt :: ((a,b) -> (d,e)) -> ((a,b,c) -> (d,e,c))
-adapt f = \ (a,b,c) -> let (d,e) = f (a,b) in (d,e,c)
+adapt f (a,b,c) = let (d,e) = f (a,b) in (d,e,c)
 
 increaseAim :: Int -> Position2 -> Position2
 increaseAim amount (h,v,a) = (h,v, a + amount)
