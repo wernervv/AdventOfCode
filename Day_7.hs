743d9cd6c0e7863753570f45514f5bd06796ce54
Ville Werner <ville.werner@gmail.com>
2021-12-15 21:59:24 +0200

    Day 7 second part done

--- Day_7.hs
+++ Day_7.hs
@@ -6,14 +6,14 @@ input = map (read :: String -> Int) . splitOn "," <$> readFile "day_7_input.txt"
 guess :: [Int] -> Int
 guess input = (maximum input + minimum input) `div` 2
 
-tweak :: Int -> Int -> [Int] -> Int
-tweak value currentCost positions
-  | leftShiftCost < currentCost = tweak (value - 1) leftShiftCost positions
-  | rightShiftCost < currentCost = tweak (value + 1) rightShiftCost positions
+tweak :: Int -> Int -> (Int -> [Int] -> Int) -> [Int] -> Int
+tweak value currentCost f positions
+  | leftShiftCost < currentCost = tweak (value - 1) leftShiftCost f positions
+  | rightShiftCost < currentCost = tweak (value + 1) rightShiftCost f positions
   | otherwise = currentCost
     where
-      leftShiftCost = cost (value - 1) positions
-      rightShiftCost = cost (value + 1) positions
+      leftShiftCost = f (value - 1) positions
+      rightShiftCost = f (value + 1) positions
 
 cost :: Int -> [Int] -> Int
 cost value = sum . map (abs . (value -))
@@ -22,4 +22,19 @@ firstPart :: IO Int
 firstPart =
   do nums <- input
      let initialValue = guess nums
-     return $ tweak initialValue (cost initialValue nums) nums
+     return $ tweak initialValue (cost initialValue nums) cost nums
+
+cost2 :: Int -> [Int] -> Int
+cost2 value = sum . map (spanCost value)
+
+spanCost :: Int -> Int -> Int
+spanCost from to = sum [1..(abs (to - from))]
+
+secondPart :: IO Int
+secondPart =
+  do nums <- input
+     let initialValue = guess nums
+     return $ tweak initialValue (cost2 initialValue nums) cost2 nums
+
+testInput :: [Int]
+testInput = [16,1,2,0,4,2,7,1,2,14]
