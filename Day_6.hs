f48d085bf9632e571f6d5efd64f4e7dff00d5945
Ville Werner <ville.werner@gmail.com>
2021-12-15 20:01:42 +0200

    Day 6 second part done

--- Day_6.hs
+++ Day_6.hs
@@ -1,5 +1,7 @@
 import Data.List.Split
 
+type CycleState = (Int,Int,Int,Int,Int,Int,Int,Int,Int)
+
 input :: IO [Int]
 input = map (read :: String -> Int) . splitOn "," <$> readFile "day_6_input.txt"
 
@@ -15,6 +17,20 @@ day (x:xs) =
 
 firstPart :: IO Int
 firstPart = sum . map allSubsequentCount . initInput 80 <$> input
+
+day2 :: CycleState -> CycleState
+day2 (ze,on,tw,th,fo,fi,si,se,ei) = (on, tw, th, fo, fi, si, se + ze, ei, ze)
+
+initCycleState :: [Int] -> CycleState
+initCycleState input = (f 0, f 1, f 2, f 3, f 4, f 5, f 6, f 7, f 8)
+  where
+    f n = length $ filter (== n) input
+
+allBasedOnCycleState :: CycleState -> Int
+allBasedOnCycleState (ze,on,tw,th,fo,fi,si,se,ei) = ze + on + tw + th + fo + fi + si + se + ei
+
+secondPart :: IO Int
+secondPart = allBasedOnCycleState . flip (foldl (flip ($))) (replicate 256 day2) . initCycleState <$> input
 
 allSubsequentCount :: (Int, Int) -> Int
 allSubsequentCount (ctr, daysLeft) =
