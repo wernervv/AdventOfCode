34a3ff98c320ca0254cbe9c65fab6a970dad8ef8
Ville Werner <ville.werner@gmail.com>
2021-12-22 17:48:35 +0200

    Day 17 second part done

--- Day_17.hs
+++ Day_17.hs
@@ -1,3 +1,4 @@
+import Data.Bifunctor(bimap)
 import Data.List.Split(splitOn)
 
 type Area = (Int,Int,Int,Int)
@@ -23,8 +24,57 @@ extractRange s =
 start :: Coord
 start = (0,0)
 
-isInArea :: Coord -> Area -> Bool
-isInArea (x,y) (x1,x2,y1,y2) = x >= x1 && x <= x2 && y >= y1 && y <= y2
+maxYVelocity :: Area -> Int
+maxYVelocity (_,_,y,_) =
+  let startY = snd start
+  in startY - y -1
+
+highestPoint :: Int -> Int
+highestPoint yVelocity = snd start + sum [1..yVelocity]
+
+firstPart :: IO Int
+firstPart = highestPoint . maxYVelocity <$> input
+
+minYVelocity :: Area -> Int
+minYVelocity (_,_,y,_) = y - snd start
+
+maxXVelocity :: Area -> Int
+maxXVelocity (_,x,_,_) = x - fst start
+
+minXVelocity :: Area -> Int
+minXVelocity (x,_,_,_) = helper 1 (x - fst start)
+  where
+    helper stepSize stillUncovered =
+      let newUncovered = stillUncovered - stepSize
+          newStepSize = stepSize + 1 -- target area assumed in positive x direction
+      in if newUncovered <= 0 then stepSize else helper newStepSize newUncovered
+
+allCandidateVelocities :: Area -> [Velocity]
+allCandidateVelocities a = [(vx,vy) | vx <- [(minXVelocity a) .. (maxXVelocity a)], vy <- [(minYVelocity a) .. (maxYVelocity a)]]
+
+minmax :: Char -> Area -> Coord
+minmax c (x1,x2,y1,y2) =
+  case c of
+    'x' -> (x1,x2)
+    'y' -> (y1,y2)
+    _   -> (0,0)
+
+both :: (a -> b) -> (a,a) -> (b,b)
+both f = bimap f f
+
+isUnderTarget :: Coord -> Area -> Bool
+isUnderTarget (_,y) (_,_,ay,_) = y < ay
+
+isInArea :: Area -> Coord -> Bool
+isInArea (x1,x2,y1,y2) (x,y) = x >= x1 && x <= x2 && y >= y1 && y <= y2
+
+hasHitTarget :: Area -> [(Coord,Velocity)] -> Bool
+hasHitTarget area = any (isInArea area . fst)
+
+isValidInitialVelocity :: Velocity -> Area -> Bool
+isValidInitialVelocity v a =
+  let trajectory = takeWhile (not . (`isUnderTarget` a) . fst) $ iterate stepTrajectory (start,v)
+  in hasHitTarget a trajectory
 
 stepTrajectory :: (Coord,Velocity) -> (Coord,Velocity)
 stepTrajectory ((x,y),(vx,vy)) =
@@ -37,13 +87,7 @@ stepTrajectory ((x,y),(vx,vy)) =
       newVy = vy-1
   in ((newX,newY),(newVx,newVy))
 
-maxYVelocity :: Area -> Int
-maxYVelocity (_,_,y,_) =
-  let startY = snd start
-  in startY - y -1
-
-highestPoint :: Int -> Int
-highestPoint yVelocity = snd start + sum [1..yVelocity]
-
-firstPart :: IO Int
-firstPart = highestPoint . maxYVelocity <$> input
+secondPart :: IO Int
+secondPart =
+  do area <- input
+     return $ length . filter (`isValidInitialVelocity` area) $ allCandidateVelocities area
