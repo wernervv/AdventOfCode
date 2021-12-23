b1978b15de5f54fad35a1af042e289eee710bd12
Ville Werner <ville.werner@tuni.fi>
2021-12-23 21:32:14 +0200

    Day 18 first part done

--- Day_18.hs
+++ Day_18.hs
@@ -3,13 +3,17 @@ import Data.Char(intToDigit)
 import Data.List.Split(splitOn)
 import Data.Maybe(fromMaybe)
 
-data SnailfishNumberElement = N Int | P (SnailfishNumberElement,SnailfishNumberElement)
-type SnailfishNumber = (SnailfishNumberElement,SnailfishNumberElement)
+data SnailfishNumberElement a = N a | P (SnailfishNumberElement a,SnailfishNumberElement a)
+type SnailfishNumber = (SnailfishNumberElement Int,SnailfishNumberElement Int)
 type PathToElement = String
 
-instance Show SnailfishNumberElement where
+instance Show a => Show (SnailfishNumberElement a) where
     show (N val) = " N " ++ show val
     show (P (val1,val2)) = " P (" ++ show val1 ++ "," ++ show val2 ++ ")"
+
+instance Functor SnailfishNumberElement where
+  fmap f (N val) = N (f val)
+  fmap f (P (val1,val2)) = P (fmap f val1,fmap f val2)
 
 input :: IO [SnailfishNumber]
 input = map parseAsSnailfishNumber . lines <$> readFile "day_18_input.txt"
@@ -49,7 +53,7 @@ both f = bimap f f
 isDigit :: Char -> Bool
 isDigit = (`elem` "0123456789")
 
-parseAsSnailfishNumberElement :: String -> SnailfishNumberElement
+parseAsSnailfishNumberElement :: String -> SnailfishNumberElement Int
 parseAsSnailfishNumberElement s = if isDigit . head $ s then N $ (read :: String -> Int) [head s] else P $ both parseAsSnailfishNumberElement (splitInTwo s)
 
 parseAsSnailfishNumber :: String -> SnailfishNumber
@@ -70,7 +74,7 @@ intAsString n =
     then intAsString (n `div` 10) ++ intAsString (n `mod` 10)
     else [intToDigit n]
 
-sfnelAsString :: SnailfishNumberElement -> String
+sfnelAsString :: SnailfishNumberElement Int -> String
 sfnelAsString (N val) = intAsString val
 sfnelAsString (P (val1,val2)) = "[" ++ sfnelAsString val1 ++ "," ++ sfnelAsString val2 ++ "]"
 
@@ -86,7 +90,7 @@ pickJust (mval1,mval2) =
     orig@(Just _) -> orig
     Nothing -> mval2
 
-giveExploding :: SnailfishNumber -> Maybe SnailfishNumberElement
+giveExploding :: SnailfishNumber -> Maybe (SnailfishNumberElement Int)
 giveExploding = pickJust . both (helper 3)
   where
     helper 0 (N _) = Nothing
@@ -102,51 +106,118 @@ givePathToExploding (l,r) = reverse <$> pickJust (helper 3 "L" l, helper 3 "R" r
     helper n acc (N _) = Nothing
     helper n acc (P (val1,val2)) = pickJust (helper (n-1) ('L':acc) val1, helper (n-1) ('R':acc) val2)
 
-followPathElement :: PathToElement -> SnailfishNumberElement -> Maybe SnailfishNumberElement
+followPathElement :: PathToElement -> SnailfishNumberElement Int -> Maybe (SnailfishNumberElement Int)
 followPathElement [] sfnel = Just sfnel
 followPathElement (choice:rest) (N _) = Nothing
 followPathElement (choice:rest) (P (val1,val2)) = if choice == 'L' then followPathElement rest val1 else followPathElement rest val2
 
-followPath :: PathToElement -> SnailfishNumber -> Maybe SnailfishNumberElement
+followPath :: PathToElement -> SnailfishNumber -> Maybe (SnailfishNumberElement Int)
 followPath [] sfn = Nothing
 followPath (choice:rest) (l,r) = if choice == 'L' then followPathElement rest l else followPathElement rest r
 
-rightmostRegNumPathEnding :: SnailfishNumberElement -> PathToElement
-rightmostRegNumPathEnding sfnel =
+pathToRegNumInGivenDirection :: Char -> SnailfishNumberElement Int -> PathToElement
+pathToRegNumInGivenDirection c sfnel =
   case sfnel of
     (N _) -> []
-    (P (_,rval)) -> 'R' : rightmostRegNumPathEnding rval
+    (P (lval,rval)) -> c : pathToRegNumInGivenDirection c (if c == 'L' then lval else rval)
 
-giveRightmostRegularBranch :: PathToElement -> SnailfishNumber -> PathToElement
-giveRightmostRegularBranch pte sfn =
+continuePathToRegNum :: Char -> PathToElement -> SnailfishNumber -> PathToElement
+continuePathToRegNum c pte sfn =
   let msfnel = followPath pte sfn
-      rightMost = msfnel >>= return . rightmostRegNumPathEnding
-  in maybe [] (pte ++) rightMost
+      flippedDirection = if c == 'L' then 'R' else 'L'
+      rightOrLeftmost = msfnel >>= return . pathToRegNumInGivenDirection flippedDirection
+  in maybe [] (pte ++) rightOrLeftmost
 
-givePathToFirstRegularLeft :: PathToElement -> SnailfishNumber -> PathToElement
-givePathToFirstRegularLeft pte sfn = maybe [] ((`giveRightmostRegularBranch` sfn) . reverse) (helper [] sfn (reverse pte))
+givePathToNextRegNumInGivenDirection :: Char -> PathToElement -> SnailfishNumber -> PathToElement
+givePathToNextRegNumInGivenDirection c pte sfn = maybe [] (flip (continuePathToRegNum c) sfn . reverse) (helper c sfn (reverse pte))
   where
-    helper acc sfn [] = Nothing
-    helper acc sfn (choice:rest) = if choice == 'L' then helper acc sfn rest else Just $ 'L':rest
+    helper c sfn [] = Nothing
+    helper c sfn (choice:rest) = if choice == c then helper c sfn rest else Just $ c:rest
 
-pathToFirstToBeAdded :: SnailfishNumber -> PathToElement
-pathToFirstToBeAdded sfn = fromMaybe [] $ givePathToExploding sfn >>= return . (`givePathToFirstRegularLeft` sfn)
+insertBetween :: b -> (a,c) -> (a,b,c)
+insertBetween b (a,c) = (a,b,c)
+
+pathsToNumsParticipatingInExplosion :: SnailfishNumber -> (PathToElement,PathToElement,PathToElement)
+pathsToNumsParticipatingInExplosion sfn =
+  let exploding = fromMaybe [] $ givePathToExploding sfn
+  in insertBetween exploding $ both (\ c -> givePathToNextRegNumInGivenDirection c exploding sfn) ('L','R')
+
+updateByPathElement :: PathToElement -> (SnailfishNumberElement Int -> SnailfishNumberElement Int) -> SnailfishNumberElement Int -> SnailfishNumberElement Int
+updateByPathElement [] f sfnel = f sfnel
+updateByPathElement (choice:rest) f orig@(N _) = orig
+updateByPathElement (choice:rest) f (P (val1,val2)) = if choice == 'L' then P (updateByPathElement rest f val1,val2) else P (val1,updateByPathElement rest f val2)
+
+updateByPath :: PathToElement -> (SnailfishNumberElement Int -> SnailfishNumberElement Int) -> SnailfishNumber -> SnailfishNumber
+updateByPath [] f orig = orig
+updateByPath (choice:rest) f (val1,val2) = if choice == 'L' then (updateByPathElement rest f val1,val2) else (val1,updateByPathElement rest f val2)
+
+extractPair :: SnailfishNumberElement Int -> (Int,Int)
+extractPair sfnel =
+  case sfnel of
+    P (N val1, N val2) -> (val1,val2)
+    _ -> (0,0)
+
+giveExplodingNums :: PathToElement -> SnailfishNumber -> (Int,Int)
+giveExplodingNums pte sfn = extractPair . fromMaybe (P (N 0, N 0)) $ followPath pte sfn
 
 explodeSfn :: SnailfishNumber -> SnailfishNumber
-explodeSfn = undefined
+explodeSfn sfn =
+  let (leftPath, explodingPath, rightPath) = pathsToNumsParticipatingInExplosion sfn
+      (leftExploding,rightExploding) = giveExplodingNums explodingPath sfn
+  in updateByPath leftPath (fmap (+leftExploding)) (updateByPath explodingPath (const (N 0)) (updateByPath rightPath (fmap (+rightExploding)) sfn))
 
-hasRegularNumberGreaterThanNine :: SnailfishNumberElement -> Bool
+hasRegularNumberGreaterThanNine :: SnailfishNumberElement Int -> Bool
 hasRegularNumberGreaterThanNine (N val) = val > 9
 hasRegularNumberGreaterThanNine (P (val1,val2)) = hasRegularNumberGreaterThanNine val1 || hasRegularNumberGreaterThanNine val2
 
 needsToSplit :: SnailfishNumber -> Bool
 needsToSplit = uncurry (||) . both hasRegularNumberGreaterThanNine
 
+pathToSplittingElement :: SnailfishNumberElement Int -> Maybe PathToElement
+pathToSplittingElement = fmap reverse . helper []
+  where
+    helper :: PathToElement -> SnailfishNumberElement Int -> Maybe PathToElement
+    helper acc (N val) = if val > 9 then Just acc else Nothing
+    helper acc (P (val1,val2)) = pickJust (helper ('L':acc) val1,helper ('R':acc) val2)
+
+pathToSplitting :: SnailfishNumber -> PathToElement
+pathToSplitting = fromMaybe [] . pickJust . bimap (fmap ('L':)) (fmap ('R':)) . both pathToSplittingElement
+
+splitNumber :: SnailfishNumberElement Int -> SnailfishNumberElement Int
+splitNumber (N val) =
+  let decimalNumber = fromIntegral val / 2
+      lval = floor decimalNumber
+      rval = ceiling decimalNumber
+  in P (N lval,N rval)
+splitNumber (P _) = P (N 0,N 0)
+
 splitSfn :: SnailfishNumber -> SnailfishNumber
-splitSfn = undefined
+splitSfn sfn =
+  let splitPath = pathToSplitting sfn
+  in updateByPath splitPath splitNumber sfn
+
+magnitudeElement :: SnailfishNumberElement Int -> Int
+magnitudeElement (N val) = val
+magnitudeElement (P (val1,val2)) = 3 * magnitudeElement val1 + 2 * magnitudeElement val2
+
+magnitude :: SnailfishNumber -> Int
+magnitude = uncurry (+) . bimap ((3*) . magnitudeElement) ((2*) . magnitudeElement)
+
+firstPart :: IO Int
+firstPart = magnitude . foldl1 addSnailfishNumbers <$> input
 
 testInputs :: [String]
 testInputs = ["[1,2]", "[[1,2],3]", "[9,[8,7]]", "[[1,9],[8,5]]", "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]", "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]", "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"]
 
-addingExample :: String
-addingExample = "[1,[[[[5,6],4],3],2]]"
+addingExample :: [String]
+addingExample =
+  ["[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"
+  ,"[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+  ,"[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]"
+  ,"[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]"
+  ,"[7,[5,[[3,8],[1,4]]]]"
+  ,"[[2,[2,2]],[8,[8,1]]]"
+  ,"[2,9]"
+  ,"[1,[[[9,3],9],[[9,0],[0,7]]]]"
+  ,"[[[5,[7,4]],7],1]"
+  ,"[[[[4,2],2],6],[8,7]]"]
