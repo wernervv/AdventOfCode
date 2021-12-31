b93beb04b9554e1addcbb76dbd8b5fd4cfe402ac
Ville Werner <ville.werner@tuni.fi>
2021-12-31 16:58:20 +0200

    Day 22 first part done

--- Day_22.hs
+++ Day_22.hs
@@ -1,6 +1,7 @@
 import Data.List.Split(splitOn)
 
 type Instruction = (Bool,(Int,Int),(Int,Int),(Int,Int)) -- turn on, x, y, z
+type Coord = (Int,Int,Int)
 
 input :: IO [String]
 input = lines <$> readFile "day_22_input.txt"
@@ -20,3 +21,27 @@ isPartOfInitialization (_,rx,ry,rz) = rangeIsPartOfInitialization rx && rangeIsP
 
 instructionsFirstPart :: IO [Instruction]
 instructionsFirstPart = filter isPartOfInitialization . map parseInstruction <$> input
+
+isOn :: Instruction -> Bool
+isOn (b,_,_,_) = b
+
+isInRange :: (Int,Int) -> Int -> Bool
+isInRange (minC,maxC) c = c >= minC && c <= maxC
+
+instructionApplies :: Instruction -> Coord -> Bool
+instructionApplies (_,rx,ry,rz) (x,y,z) = isInRange rx x && isInRange ry y && isInRange rz z
+
+lastInstructionConcerning :: [Instruction] -> Coord -> Maybe Instruction
+lastInstructionConcerning instr c =
+  let allConcerningCoord = filter (`instructionApplies` c) instr
+  in if null allConcerningCoord then Nothing else Just . last $ allConcerningCoord
+
+isOnAfterInstructions :: [Instruction] -> Coord -> Bool
+isOnAfterInstructions instructions c = maybe False isOn (lastInstructionConcerning instructions c)
+
+firstPart :: IO Int
+firstPart =
+  do instructions <- instructionsFirstPart
+     return . length . filter (isOnAfterInstructions instructions) $ [ (x,y,z) | x <- initializationRange, y <- initializationRange, z <- initializationRange]
+       where
+         initializationRange = [-50..50]
