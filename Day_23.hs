ec8a80e21388914e327709137c014bc416a89949
Ville Werner <ville.werner@gmail.com>
2022-01-11 15:12:01 +0200

    Day 23 second part done

--- Day_23.hs
+++ Day_23.hs
@@ -9,9 +9,17 @@ type Estimate = (Burrow,Int)
 type Amphipod = (Char,Int)
 type Dodges = [Amphipod]
 
+data BurrowSpace2 a = H a | S (a,a,a,a)
+type BurrowUnit2 = (Int, BurrowSpace2 Char)
+type Burrow2 = ([BurrowUnit2],Int)
+
 instance Show a => Show (BurrowSpace a) where
   show (HW val) = show val
   show (SR (val1,val2)) = "(" ++ show val1 ++ "," ++ show val2 ++ ")"
+
+instance Show a => Show (BurrowSpace2 a) where
+  show (H val) = show val
+  show (S (v1,v2,v3,v4)) = "(" ++ show v1 ++ "," ++ show v2 ++ "," ++ show v3 ++ "," ++ show v4 ++ ")"
 
 input :: IO [String]
 input = lines <$> readFile "day_23_input.txt"
@@ -28,11 +36,37 @@ parseAsBurrow ls =
            | b == '#' && a == '.' = [(n,HW a)]
            | otherwise = []
 
+parseAsBurrow2 :: [String] -> Burrow2
+parseAsBurrow2 = addMissingPart . parseAsBurrow
+
+missing :: [(Char,Char)]
+missing = [('D','D'),('C','B'),('B','A'),('A','C')]
+
+addToRoom :: BurrowUnit -> [(Char,Char)] -> BurrowUnit2
+addToRoom (x,HW val) _ = (x,H val)
+addToRoom (x,SR (a,d)) ls = let (b,c) = head ls in (x,S (a,b,c,d))
+
+afterOneUnit :: BurrowUnit -> [(Char,Char)] -> [(Char,Char)]
+afterOneUnit (_,HW _) = id
+afterOneUnit (_,SR _) = tail
+
+listState :: [BurrowUnit] -> [[(Char,Char)]]
+listState = (missing :) . scanl (flip afterOneUnit) missing
+
+addMissingPart :: Burrow -> Burrow2
+addMissingPart (lbu,count) = (zipWith addToRoom lbu (listState lbu),count)
+
 lockedAmphipods :: Burrow -> [Amphipod]
 lockedAmphipods = mapMaybe getAmphipod . hallWay
 
+lockedAmphipods2 :: Burrow2 -> [Amphipod]
+lockedAmphipods2 = mapMaybe getAmphipod2 . hallWay2
+
 movableAmphipods :: Burrow -> [Amphipod]
 movableAmphipods = mapMaybe getAmphipod . sideRooms
+
+movableAmphipods2 :: Burrow2 -> [Amphipod]
+movableAmphipods2 = mapMaybe getAmphipod2 . sideRooms2
 
 constrictOne :: (Int,Int) -> Amphipod -> Burrow -> (Int,Int)
 constrictOne (small,big) a@(_,x) b =
@@ -66,6 +100,20 @@ allToTargets b =
           else case moveToGiven x a b of
                  Nothing -> helper rest b
                  (Just newB) -> allToTargets newB
+
+allToTargets2 :: Burrow2 -> Burrow2
+allToTargets2 b =
+  let as = movableAmphipods2 b ++ lockedAmphipods2 b
+      aAndT = zip as (map (`getTargetRoomXCoord2` b) as)
+  in helper aAndT b
+    where
+      helper [] b = b
+      helper ((a,x):rest) b =
+        if snd a == x
+          then helper rest b
+          else case moveToGiven2 x a b of
+                 Nothing -> helper rest b
+                 (Just newB) -> allToTargets2 newB
 
 -- branch :: Estimate -> Amphipod -> [Estimate]
 -- branch (b,e) a = map tagWithEstimate . mapMaybe (\ (i,_) -> moveToGiven i a b) $ hallWay b
@@ -254,6 +302,13 @@ hallWay (b:bs,_) =
     h@(_,HW _) -> h : hallWay (bs,0)
     _ -> hallWay (bs,0)
 
+hallWay2 :: Burrow2 -> [BurrowUnit2]
+hallWay2 ([],_) = []
+hallWay2 (b:bs,_) =
+  case b of
+    h@(_,H _) -> h : hallWay2 (bs,0)
+    _ -> hallWay2 (bs,0)
+
 sideRooms :: Burrow -> [BurrowUnit]
 sideRooms ([],_) = []
 sideRooms (b:bs,_) =
@@ -261,13 +316,29 @@ sideRooms (b:bs,_) =
     s@(_,SR _) -> s : sideRooms (bs,0)
     _ -> sideRooms (bs,0)
 
+sideRooms2 :: Burrow2 -> [BurrowUnit2]
+sideRooms2 ([],_) = []
+sideRooms2 (b:bs,_) =
+  case b of
+    s@(_,S _) -> s : sideRooms2 (bs,0)
+    _ -> sideRooms2 (bs,0)
+
 isInHallWay :: Burrow -> Amphipod -> Bool
 isInHallWay b (_,x) = any (\ (pos,_) -> pos == x) $ hallWay b
+
+isInHallWay2 :: Burrow2 -> Amphipod -> Bool
+isInHallWay2 b (_,x) = any (\ (pos,_) -> pos == x) $ hallWay2 b
 
 getTargetRoomXCoord :: Amphipod -> Burrow -> Int
 getTargetRoomXCoord (c,_) b =
   let srCoords = map fst $ sideRooms b
       zipped = zip "ABCD" srCoords
+  in snd . head $ filter (\ p -> fst p == c) zipped
+
+getTargetRoomXCoord2 :: Amphipod -> Burrow2 -> Int
+getTargetRoomXCoord2 (c,_) b =
+  let sCoords = map fst $ sideRooms2 b
+      zipped = zip "ABCD" sCoords
   in snd . head $ filter (\ p -> fst p == c) zipped
 
 routeIsBlocked :: Int -> Int -> Burrow -> Bool
@@ -280,6 +351,16 @@ routeIsBlocked target current b =
         | otherwise = []
   in not . null . mapMaybe getAmphipod $ blockingBurrowUnits
 
+routeIsBlocked2 :: Int -> Int -> Burrow2 -> Bool
+routeIsBlocked2 target current b =
+  let h = hallWay2 b
+      delta = current - target
+      blockingBurrowUnits2
+        | delta > 0 = filter (\ (x,_) -> let tmpDelta = x - target in tmpDelta >= 0 && tmpDelta < delta) h
+        | current < target = filter (\ (x,_) -> let tmpDelta = x - target in tmpDelta <= 0 && tmpDelta > delta) h
+        | otherwise = []
+  in not . null . mapMaybe getAmphipod2 $ blockingBurrowUnits2
+
 getSideRoom :: Int -> Burrow -> Maybe BurrowUnit
 getSideRoom x (b,_) =
   let bu = head . filter (\ (n,_) -> n == x) $ b
@@ -287,9 +368,24 @@ getSideRoom x (b,_) =
        val@(_,SR _) -> Just val
        _ -> Nothing
 
+getSideRoom2 :: Int -> Burrow2 -> Maybe BurrowUnit2
+getSideRoom2 x (b,_) =
+  let bu = head . filter (\ (n,_) -> n == x) $ b
+  in case bu of
+       val@(_,S _) -> Just val
+       _ -> Nothing
+
 stepsToSR :: BurrowUnit -> Int
 stepsToSR (_,HW _) = 0
 stepsToSR (_,SR (_,c)) = if c == '.' then 2 else 1
+
+stepsToS :: BurrowUnit2 -> Int
+stepsToS (_,H _) = 0
+stepsToS (_,S (_,c2,c3,c4))
+  | c4 == '.' = 4
+  | c3 == '.' = 3
+  | c2 == '.' = 2
+  | otherwise = 1
 
 getMoveCost :: Char -> Int -> Int
 getMoveCost c n =
@@ -305,6 +401,9 @@ getMoveCost c n =
 updateHW :: BurrowSpace Char -> Char -> BurrowSpace Char
 updateHW = const HW
 
+updateH :: BurrowSpace2 Char -> Char -> BurrowSpace2 Char
+updateH = const H
+
 updateSR :: BurrowSpace Char -> Char -> BurrowSpace Char
 updateSR (SR (x,y)) c
   | c == '.' = if x == '.' then SR (x,c) else SR (c,y)
@@ -314,23 +413,56 @@ updateSR (SR (x,y)) c
       else SR (c,y)
 updateSR _ _ = SR ('#','#')
 
+updateS :: BurrowSpace2 Char -> Char -> BurrowSpace2 Char
+updateS (S (a,b,c,d)) ch
+  | ch == '.' = if a /= '.'
+                  then S ('.',b,c,d)
+                  else if b /= '.'
+                         then S (a,'.',c,d)
+                         else if c /= '.'
+                                then S (a,b,'.',d)
+                                else S (a,b,c,'.')
+  | d == '.' = S (a,b,c,ch)
+  | c == '.' = S (a,b,ch,d)
+  | b == '.' = S (a,ch,c,d)
+  | otherwise = S (ch,b,c,d)
+updateS _ _ = S ('#','#','#','#')
+
 updateBS :: BurrowSpace Char -> Char -> BurrowSpace Char
 updateBS bs c = case bs of
                   hw@(HW _) -> updateHW hw c
                   sr@(SR _) -> updateSR sr c
 
+updateBS2 :: BurrowSpace2 Char -> Char -> BurrowSpace2 Char
+updateBS2 bs c = case bs of
+                   h@(H _) -> updateH h c
+                   s@(S _) -> updateS s c
+
 updateTarget :: Int -> Char -> [BurrowUnit] -> [BurrowUnit]
 updateTarget target c = map (\ orig@(x,bs) -> if x == target then (x,updateBS bs c) else orig)
 
+updateTarget2 :: Int -> Char -> [BurrowUnit2] -> [BurrowUnit2]
+updateTarget2 target c = map (\ orig@(x,bs) -> if x == target then (x,updateBS2 bs c) else orig)
+
 makeMove :: Int -> Amphipod -> [BurrowUnit] -> [BurrowUnit]
 makeMove target (c,x) = updateTarget x '.' . updateTarget target c
+
+makeMove2 :: Int -> Amphipod -> [BurrowUnit2] -> [BurrowUnit2]
+makeMove2 target (c,x) = updateTarget2 x '.' . updateTarget2 target c
 
 cannotEnter :: Char -> BurrowUnit -> Bool
 cannotEnter c (_,SR (c1,c2)) = let allowed = ['.',c] in notElem c1 allowed || notElem c2 allowed
 cannotEnter _ (_,HW _) = False
 
+cannotEnter2 :: Char -> BurrowUnit2 -> Bool
+cannotEnter2 c (_,S (c1,c2,c3,c4)) = let allowed = ['.',c] in notElem c1 allowed || notElem c2 allowed || notElem c3 allowed || notElem c4 allowed
+cannotEnter2 _ (_,H _) = False
+
 cannotEnterTargetRoom :: Amphipod -> Int -> Burrow -> Bool
 cannotEnterTargetRoom (c,_) target = cannotEnter c . head . filter (\ p -> fst p == target) . fst
+
+cannotEnterTargetRoom2 :: Amphipod -> Int -> Burrow2 -> Bool
+cannotEnterTargetRoom2 (c,_) target = cannotEnter2 c . head . filter (\ p -> fst p == target) . fst
 
 getStepsToHallway :: Amphipod -> Burrow -> Int
 getStepsToHallway a b =
@@ -338,6 +470,19 @@ getStepsToHallway a b =
   in case sr of
        (_,SR (c,_)) -> if c == '.' then 2 else 1
        (_,HW _) -> 0
+
+getStepsToHallway2 :: Amphipod -> Burrow2 -> Int
+getStepsToHallway2 a b =
+  let s = head . filter (\ p -> fst p == snd a) . fst $ b
+  in case s of
+       (_,S (c1,c2,c3,_)) -> if c3 == '.'
+                               then 4
+                               else if c2 == '.'
+                                      then 3
+                                      else if c1 == '.'
+                                             then 2
+                                             else 1
+       (_,H _) -> 0
 
 moveToGiven :: Int -> Amphipod -> Burrow -> Maybe Burrow
 moveToGiven target a@(c,x) b@(bs,cost) =
@@ -350,8 +495,22 @@ moveToGiven target a@(c,x) b@(bs,cost) =
              newBurrowUnits = makeMove target a bs
          in Just (newBurrowUnits,newCost)
 
+moveToGiven2 :: Int -> Amphipod -> Burrow2 -> Maybe Burrow2
+moveToGiven2 target a@(c,x) b@(bs,cost) =
+  if routeIsBlocked2 target x b || cannotEnterTargetRoom2 a target b
+    then Nothing
+    else let mS = getSideRoom2 target b
+             distance = abs (target - x) + maybe 0 stepsToS mS
+             stepsToHallway = getStepsToHallway2 a b
+             newCost = cost + getMoveCost c (stepsToHallway + distance)
+             newBurrowUnits2 = makeMove2 target a bs
+         in Just (newBurrowUnits2,newCost)
+
 isTargetRoom :: BurrowUnit -> Amphipod -> Burrow -> Bool
 isTargetRoom (x,_) a b = getTargetRoomXCoord a b == x
+
+isTargetRoom2 :: BurrowUnit2 -> Amphipod -> Burrow2 -> Bool
+isTargetRoom2 (x,_) a b = getTargetRoomXCoord2 a b == x
 
 isHomogenous :: BurrowUnit -> Bool
 isHomogenous (_,HW _) = False
@@ -359,10 +518,19 @@ isHomogenous (_,SR (a,b))
   | a == '.' = True
   | otherwise = a == b
 
+isHomogenous2 :: BurrowUnit2 -> Bool
+isHomogenous2 (_,H _) = False
+isHomogenous2 (_,S (c1,c2,c3,c4)) = all (`elem` ['.',c4]) [c1,c2,c3]
+
 alreadyCorrect :: Amphipod -> Burrow -> Bool
 alreadyCorrect a b =
   let currentRoom = head . filter (\ p -> fst p == snd a) $ sideRooms b
   in isTargetRoom currentRoom a b && isHomogenous currentRoom
+
+alreadyCorrect2 :: Amphipod -> Burrow2 -> Bool
+alreadyCorrect2 a b =
+  let currentRoom = head . filter (\ p -> fst p == snd a) $ sideRooms2 b
+  in isTargetRoom2 currentRoom a b && isHomogenous2 currentRoom
 
 allMoves :: Amphipod -> Burrow -> [Burrow]
 allMoves a b =
@@ -374,6 +542,16 @@ allMoves a b =
               then []
               else case movedToTarget of Just val -> [val]; Nothing -> mapMaybe ((\ x -> moveToGiven x a b) . fst) . hallWay $ b
 
+allMoves2 :: Amphipod -> Burrow2 -> [Burrow2]
+allMoves2 a b =
+  let targetRoom = getTargetRoomXCoord2 a b
+      movedToTarget = moveToGiven2 targetRoom a b
+  in if isInHallWay2 b a
+       then case movedToTarget of Just val -> [val]; Nothing -> []
+       else if alreadyCorrect2 a b
+              then []
+              else case movedToTarget of Just val -> [val]; Nothing -> mapMaybe ((\ x -> moveToGiven2 x a b) . fst) . hallWay2 $ b
+
 getAmphipod :: BurrowUnit -> Maybe Amphipod
 getAmphipod (x,bs) =
   case bs of
@@ -384,13 +562,33 @@ getAmphipod (x,bs) =
                              else Just (c2,x)
                       else Just (c1,x)
 
+getAmphipod2 :: BurrowUnit2 -> Maybe Amphipod
+getAmphipod2 (x,bs) =
+  case bs of
+    (H c) -> if c == '.' then Nothing else Just (c,x)
+    (S (c1,c2,c3,c4)) -> if c1 == '.'
+                           then if c2 == '.'
+                                  then if c3 == '.'
+                                         then if c4 == '.'
+                                                then Nothing
+                                                else Just (c4,x)
+                                         else Just (c3,x)
+                                  else Just (c2,x)
+                           else Just (c1,x)
 
 isFull :: BurrowUnit -> Bool
 isFull (_,SR (c,_)) = c /= '.'
 isFull _ = False
 
+isFull2 :: BurrowUnit2 -> Bool
+isFull2 (_,S (c1,_,_,_)) = c1 /= '.'
+isFull2 _ = False
+
 isEndState :: Burrow -> Bool
 isEndState b = all (\ sr -> isHomogenous sr && isFull sr) $ sideRooms b
+
+isEndState2 :: Burrow2 -> Bool
+isEndState2 b = all (\ s -> isHomogenous2 s && isFull2 s) $ sideRooms2 b
 
 addOneToEstimates :: [Estimate] -> Estimate -> [Estimate]
 addOneToEstimates [] e = [e]
@@ -452,12 +650,45 @@ smallestEnd b =
 firstPart :: IO Int
 firstPart = smallestEnd . parseAsBurrow <$> input
 
+nextStates2 :: Burrow2 -> [Burrow2]
+nextStates2 b = concatMap (`allMoves2` b) (movableAmphipods2 b)
+
+branches :: Burrow2 -> [Burrow2] -> [Burrow2]
+branches b bs = map allToTargets2 (nextStates2 b) ++ bs
+
+findCheaper :: Int -> [Burrow2] -> Int
+findCheaper cheapest [] = cheapest
+findCheaper cheapest (b:bs) =
+  if isEndState2 b
+    then findCheaper (min cheapest (snd b)) bs
+    else findCheaper cheapest $ branches b bs
+
+findCheapestEndState :: [Burrow2] -> Int
+findCheapestEndState [] = maxBound
+findCheapestEndState (b:bs) =
+  if isEndState2 b
+    then findCheaper (snd b) bs
+    else findCheapestEndState $ branches b bs
+
+cheapestEndStateCost :: Burrow2 -> Int
+cheapestEndStateCost b = findCheapestEndState [b]
+
+secondPart :: IO Int
+secondPart = cheapestEndStateCost . parseAsBurrow2 <$> input
+
 i1 :: [String]
 i1 = ["#############"
      ,"#...........#"
      ,"###D#B#B#A###"
      ,"  #C#C#D#A#"
      ,"  #########"]
+
+{-
+              B5
+     B7       C5
+2A9       D3
+2A9  D7   C3
+-}
 
 {-
 D3 : 2A9
@@ -482,6 +713,73 @@ testInput = ["#############"
             ,"###B#C#B#D###"
             ,"  #A#D#C#A#"
             ,"  #########"]
+
+{-
+B3 : -  : C5 D5
+C5 : -  : B7
+D5 : C5 : D9 A9
+B7 : -  : C5 D5
+D9 : -  : A9
+A9 : D9 : B3
+
+B7 -> B4
+
+// B3 : -  : C5 D5
+B4 : +  : C5 D5
+C5 : -  : -
+D5 : C5 : D9 A9
+D9 : -  : A9
+A9 : D9 : B3
+
+C5 -> C7
+
+// B3 : -  : D5
+B4 : +  : D5
+D5 : -  : D9 A9
+D9 : -  : A9
+A9 : D9 : B3
+
+D9 -> D8
+
+// B3 : -  : D5
+// B4 : +  : D5
+// D5 : -  : A9
+D8 : +  : A9
+A9 : -  : B3
+
+A9 -> A10
+
+// B3 : -  : D5
+// B4 : +  : D5
+// D5 : -  : -
+D8  : +  : -
+A10 : +  : B3
+
+D8 -> D9
+
+// B3 : -  : D5
+B4  : +  : D5
+D5  : -  : -
+A10 : +  : B3
+
+D5 -> D9
+
+// B3 : -  : -
+B4  : +  : -
+A10 : +  : B3
+
+B4 -> B5
+
+B3 : -  : -
+A10 : +  : B3
+
+B3 -> B5
+
+A10 : + : -
+
+A10 -> A3
+
+-}
 
 {-
 B3 : C5 D5
