fd239b8c58d8974e23b157ccdfdd97d394c6a818
Ville Werner <ville.werner@tuni.fi>
2022-12-01 11:33:35 +0200

    Day 1 second part done

--- 2022/day_1.kt
+++ 2022/day_1.kt
@@ -4,7 +4,7 @@ val fileName = "day_1_input.txt"
 
 fun main() {
     val input = readInput()
-    val result = maxCalories(input)
+    val result = maxCalories_part_two(input)
     println(result)
 }
 
@@ -12,7 +12,7 @@ fun readInput(): List<String> {
     return File(fileName).readLines()
 }
 
-fun maxCalories(input: List<String>): Int {
+fun maxCalories_part_one(input: List<String>): Int {
     var maxVal = 0
     var current = 0
     for (line in input) {
@@ -27,4 +27,29 @@ fun maxCalories(input: List<String>): Int {
         }
     }
     return maxVal
+}
+
+fun maxCalories_part_two(input: List<String>): Int {
+    var maxVal1 = 0
+    var maxVal2 = 0
+    var maxVal3 = 0
+    var current = 0
+    for (line in input) {
+        if (line.isNullOrEmpty()) {
+            if (current > maxVal1) {
+                maxVal1 = current.also {current = maxVal1}
+            }
+            if (current > maxVal2) {
+                maxVal2 = current.also {current = maxVal2}
+            }
+            if (current > maxVal3) {
+                maxVal3 = current.also {current = maxVal3}
+            }
+            current = 0
+        }
+        else {
+            current += line.toInt()
+        }
+    }
+    return maxVal1 + maxVal2 + maxVal3
 }
