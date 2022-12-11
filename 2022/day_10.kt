ccc76069f324bc5d22d9a6571196b2c7d5247d5d
Ville Werner <ville.werner@tuni.fi>
2022-12-12 01:36:04 +0200

    Day 10 second part done

--- 2022/day_10.kt
+++ 2022/day_10.kt
@@ -12,7 +12,8 @@ fun main() {
 
     var currentInd = 0
     var cycleCount = 0
-    var registerChanges: List<Int> = emptyList()
+    // var registerChanges: List<Int> = emptyList()
+    var registerValue = 1
     var cyclesSpentOnCurrent = 0
 
     fun tick() {
@@ -23,7 +24,8 @@ fun main() {
         else {
             if (cyclesSpentOnCurrent == 1) {
                 val change = currentInstr.split(" ")[1].toInt()
-                registerChanges += change
+                // registerChanges += change
+                registerValue += change
                 cyclesSpentOnCurrent = 0
                 currentInd += 1
             }
@@ -34,14 +36,28 @@ fun main() {
         cycleCount += 1
     }
     
-    var signalStrengths: List<Int> = emptyList()
+    // var signalStrengths: List<Int> = emptyList()
     
-    while (cycleCount < relevantCycles.max()) {
-        if (relevantCycles.contains(cycleCount + 1)) {
-            val currentSignalStrength = (cycleCount + 1) * (registerChanges.fold(1, { a, b -> a + b }))
-            signalStrengths += currentSignalStrength
-        }
+    // while (cycleCount < relevantCycles.max()) {
+    //     if (relevantCycles.contains(cycleCount + 1)) {
+    //         val currentSignalStrength = (cycleCount + 1) * (registerChanges.fold(1, { a, b -> a + b }))
+    //         signalStrengths += currentSignalStrength
+    //     }
+    //     tick()
+    // }
+    // println(signalStrengths.sum())
+
+    while (cycleCount < 240) {
         tick()
+        val sprite = listOf(-1,0,1)
+        if (sprite.map { it + (cycleCount % 40) }.any { it == registerValue }) {
+            print("#")
+        }
+        else {
+            print(".")
+        }
+        if (cycleCount % 40 == 0) {
+            println()
+        }
     }
-    println(signalStrengths.sum())
 }
