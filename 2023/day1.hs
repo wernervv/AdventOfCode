817d5ed25e8ec0e1d09469df4b35c2f2e90d66bc
Ville Werner <ville.werner@gmail.com>
2023-12-03 01:42:04 +0200

    Day 1 second puzzle solved

--- 2023/day1.hs
+++ 2023/day1.hs
@@ -1,4 +1,5 @@
 import Data.Char (isDigit)
+import Control.Applicative
 
 input :: IO [String]
 input = lines <$> readFile "day1_input.txt"
@@ -14,3 +15,52 @@ combineFirstAndLast l = 10 * fi + la
 
 firstPuzzle :: IO Int
 firstPuzzle = sum <$> map (combineFirstAndLast . pluckDigits) <$> input
+
+spelledDigits :: [String]
+spelledDigits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+parseChar :: Char -> String -> Maybe String
+parseChar _ [] = Nothing
+parseChar c (x:xs) = if (c == x) then Just xs else Nothing
+
+parseString :: String -> String -> Maybe String
+parseString [] s = Just s
+parseString (c:cs) s = parseChar c s >>= parseString cs 
+
+parseSpelled :: String -> Int -> String -> Maybe Int
+parseSpelled word n s = parseString word s >>= \ _ -> Just n
+
+digitFromBeginning :: String -> Maybe Int
+digitFromBeginning [] = Nothing
+digitFromBeginning s@(c:cs) =
+  if (isDigit c)
+    then Just (read [c])
+    else foldl1 (<|>) $ map ($ s) $ zipWith parseSpelled spelledDigits [1..]
+
+parseFirstDigit :: String -> Int
+parseFirstDigit [] = 0
+parseFirstDigit s@(_:cs) = case digitFromBeginning s of
+  Just n -> n
+  Nothing -> parseFirstDigit cs
+
+digitFromEnd :: String -> Maybe Int -- operates on reversed strings
+digitFromEnd [] = Nothing
+digitFromEnd s@(c:cs) =
+  if (isDigit c)
+    then Just (read [c])
+    else foldl1 (<|>) $ map ($ s) $ zipWith parseSpelled (map reverse spelledDigits) [1..]
+
+parseLastDigit :: String -> Int
+parseLastDigit [] = 0
+parseLastDigit s = go $ reverse s
+  where
+    go s@(_:cs) = case digitFromEnd s of
+      Just n -> n
+      Nothing -> go cs
+
+getCalibrationValue :: String -> Int
+getCalibrationValue s = 10 * (parseFirstDigit s) + (parseLastDigit s)
+
+secondPuzzle :: IO Int
+secondPuzzle = sum . map getCalibrationValue <$> input
+
