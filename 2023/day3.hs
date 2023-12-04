efd1d209c31a036a48943c61ba57c22484a49521
Ville Werner <ville.werner@gmail.com>
2023-12-04 15:11:35 +0200

    Day 3 second puzzle solved

--- 2023/day3.hs
+++ 2023/day3.hs
@@ -51,3 +51,36 @@ firstPuzzle = do
   allLines <- input
   let syms = getSymbolPositions allLines
   return $ sum . map (\ (line, row) -> sum $ partNumbersInOneLine syms row line) $ zip allLines [0..]
+
+getPossibleGears :: [String] -> [(Int,Int)]
+getPossibleGears = concatMap (go 0) . zip [0..]
+  where
+    go _ (_,[]) = []
+    go i (row, (c:cs)) =
+      if (c == '*')
+        then (row,i) : go (i+1) (row, cs)
+        else go (i+1) (row, cs)
+
+isAdjacentToNumberOneLine :: Int -> (Int,Int) -> Bool
+isAdjacentToNumberOneLine x (start, end) = any (\ n -> x - n == 1 || x == n || n - x == 1) [start .. end]
+
+getAdjacentNumberPosOneLine :: String -> Int -> [(Int,Int)]
+getAdjacentNumberPosOneLine line x = filter (isAdjacentToNumberOneLine x) $ getNumberPositionsInOneLine line
+
+getAllAdjacentNumberPos :: [String] -> (Int,Int) -> [NumberLocation]
+getAllAdjacentNumberPos allLines (row, x) =
+  let relevantRows = [max 0 (row - 1) .. min (row + 1) (length allLines - 1)]
+      getAdjNumLoc allLines row x = map (\ n -> (row, n)) (getAdjacentNumberPosOneLine (allLines !! row) x)
+  in concatMap (\ row -> getAdjNumLoc allLines row x) relevantRows
+
+getNumberValueFromAll :: [String] -> NumberLocation -> Int
+getNumberValueFromAll allLines (row, p) = getNumberValue p (allLines !! row)
+
+getGearRatio :: [String] -> [NumberLocation] -> Int
+getGearRatio allLines = product . map (getNumberValueFromAll allLines)
+
+secondPuzzle :: IO Int
+secondPuzzle = do
+  allLines <- input
+  return $ sum . map (getGearRatio allLines) . filter (\ ns -> length ns == 2) . map (getAllAdjacentNumberPos allLines) $ getPossibleGears allLines
+
