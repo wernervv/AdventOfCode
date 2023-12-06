cacbc10b13f4a531079eb8cb29ca5198b3fc131b
Ville Werner <ville.werner@gmail.com>
2023-12-06 13:00:11 +0200

    Day 5 second puzzle solved

--- 2023/day5.hs
+++ 2023/day5.hs
@@ -57,6 +57,66 @@ getMinimumLocation inp =
 firstPuzzle :: IO Integer
 firstPuzzle = getMinimumLocation <$> input
 
+type ResourceRange = (String,Integer,Integer)
+
+getSeedRanges :: [String] -> [ResourceRange]
+getSeedRanges = convertToSeedRanges . map read . words . drop 1 . dropWhile (/= ':') . (!! 0)
+  where
+    convertToSeedRanges :: [Integer] -> [ResourceRange]
+    convertToSeedRanges [] = []
+    convertToSeedRanges (start:range:rest) = ("seed", start, range) : convertToSeedRanges rest
+
+biggestSubrangeWithRule :: String -> (Integer,Integer,Integer) -> ResourceRange -> ResourceRange
+biggestSubrangeWithRule resType (dest, ruleStart, ruleRange) (_, start, range) =
+  let reducedRuleRange = ruleRange - (start - ruleStart)
+      tightestRange = min range reducedRuleRange
+      newStart = start + (dest - ruleStart)
+  in (resType, newStart, tightestRange)
+
+biggestSubrangeNoRule :: String -> Map -> ResourceRange -> ResourceRange
+biggestSubrangeNoRule resType m (_, start, range) =
+  let allBiggerRuleStarts = filter (> start) . map (\ (_,ruleStart,_) -> ruleStart) $ ranges m
+      tightestRange = if (length allBiggerRuleStarts > 0) then min range (minimum allBiggerRuleStarts - start) else range
+  in (resType, start, tightestRange)
+
+reduceRange :: ResourceRange -> ResourceRange -> ResourceRange
+reduceRange (_, _, subRange) (resType, start, range) =
+  let newRange = max 0 (range - subRange)
+      newStart = start + subRange
+  in (resType, newStart, newRange)
+
+splitAndConvertRangeWithRightMap :: Map -> ResourceRange -> [ResourceRange]
+splitAndConvertRangeWithRightMap _ (_, _, 0) = []
+splitAndConvertRangeWithRightMap m rr@(_, start, range) =
+  let matchingRangeList = filter (\ (_,s,r) -> isInRange s r start) $ ranges m
+      convertedType = snd $ mapType m
+      subRange = if (length matchingRangeList > 0)
+                   then biggestSubrangeWithRule convertedType (head matchingRangeList) rr
+                   else biggestSubrangeNoRule convertedType m rr
+      remainingRange = reduceRange subRange rr
+  in subRange : splitAndConvertRangeWithRightMap m remainingRange
+
+convertOneResourceRange :: [Map] -> ResourceRange -> [ResourceRange]
+convertOneResourceRange allMaps rr@(currentType, _, _) =
+  let rightMap = head . filter (\ m -> fst (mapType m) == currentType) $ allMaps
+  in splitAndConvertRangeWithRightMap rightMap rr
+
+convertAllResourceRanges :: [Map] -> [ResourceRange] -> [ResourceRange]
+convertAllResourceRanges m rrs = rrs >>= convertOneResourceRange m
+
+getMinimumLocationFromRanges :: [Map] -> ResourceRange -> Integer
+getMinimumLocationFromRanges _ ("location", start, _) = start
+getMinimumLocationFromRanges m rr = minimum . map (getMinimumLocationFromRanges m ) $ convertOneResourceRange m rr
+
+minimumLocationFromInput :: [String] -> Integer
+minimumLocationFromInput inp =
+  let allMaps = getAllMaps inp
+      seedRanges = getSeedRanges inp
+  in minimum . map (getMinimumLocationFromRanges allMaps) $ seedRanges
+
+secondPuzzle :: IO Integer
+secondPuzzle = minimumLocationFromInput <$> input
+
 testInput :: [String]
 testInput = ["seeds: 79 14 55 13",
   "",
