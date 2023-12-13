62fecd9d26289c8884cfffd324e78f25f1af8867
Ville Werner <ville.werner@gmail.com>
2023-12-13 22:59:40 +0200

    Day 9 second puzzle solved

--- 2023/day9.hs
+++ 2023/day9.hs
@@ -23,3 +23,19 @@ extrapolate = go 0
 firstPuzzle :: IO Int
 firstPuzzle = sum . map (extrapolate . readHistory) <$> input
 
+extrapolatePrevious :: [Int] -> Int
+extrapolatePrevious ns = go [] ns
+  where
+    go accum ns =
+      if (allZeros ns)
+        then foldl (flip (-)) 0 accum
+        else go (head ns : accum) (giveDifferences ns)
+
+secondPuzzle :: IO Int
+secondPuzzle = sum . map (extrapolatePrevious . readHistory) <$> input
+
+testInput :: [String]
+testInput = ["0 3 6 9 12 15",
+  "1 3 6 10 15 21",
+  "10 13 16 21 30 45"]
+
